3679eef5b5554008ae8e6ca8a57becf3562ca1eb
Jakub Tojsl <bleskograf@gmail.com>
2023-06-22 20:30:40 +0200

    commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-text mx-3">Laravel crud <br/> produkty</div>
+    </a>
+
+
+    <hr class="sidebar-divider my-0">
+
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <span>Dashboard</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('products') }}">
+            <span>Product</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/profile">
+            <span>Profile</span></a>
+    </li>
+
+  
+    <hr class="sidebar-divider d-none d-md-block">
+
+</ul>
